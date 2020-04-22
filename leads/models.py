b843ff07ab1ef82294c63ff21a357a373885e11c
Prémices Kamasuwa <premices.tuvere@gmail.com>
2020-04-22 23:04:20 +0200

    Update models.py

--- leads/models.py
+++ leads/models.py
@@ -8,3 +8,5 @@ class Lead(models.Model):
     message = models.CharField(max_length=500, blank=True)
     owner = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    
